9326c95ae34ecb9ed8eeed3ad38de2188d0c710b
Lars Schütze <lars.schuetze@tu-dresden.de>
2025-09-30 14:25:14 +0200

    Update

--- implementations/hybrid-quantum.sh
+++ implementations/hybrid-quantum.sh
@@ -2,4 +2,4 @@
 SCRIPT_PATH=`dirname $0`
 source $SCRIPT_PATH/config.inc
 
-exec HYBRID_QUANTUM_PATH/build/bin/quantum-opt "$@"
+exec $HYBRID_QUANTUM_PATH/build/bin/quantum-opt "$@"
--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 source .venv/bin/activate
-rebench test.conf
+#rm benchmarks/compiler_generated/small/*.mlir
+#rm benchmarks/compiler_generated/medium/*.mlir
+#rm benchmarks/compiler_generated/large/*.mlir
+#rebench test.conf test-hybrid-quantum-frontend
+exec rebench test.conf test-hybrid-quantum-lift2
